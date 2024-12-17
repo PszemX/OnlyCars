6803e86ec505c80086b58ebbf6dcf4161a7c6f0f
PszemX <przemek.szumczyk@gmail.com>
2024-12-17 23:53:44 +0100

    Working main page.

---
+++ frontend/src/hooks/use-auth.ts
@@ -0,0 +1,32 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import Cookies from "js-cookie";
+import { isTokenExpired } from "@/lib/utils";
+import { jwtDecode } from "jwt-decode";
+
+interface DecodedToken {
+	nameid: string; // This will contain the userId
+	// Add other claims you expect from your token
+}
+
+export const useAuth = () => {
+	const router = useRouter();
+	const [userId, setUserId] = useState<string | null>(null);
+	const [authenticated, setAuthenticated] = useState<boolean>(false);
+
+	useEffect(() => {
+		const token = Cookies.get("token");
+		if (!token || isTokenExpired(token)) {
+			Cookies.remove("token");
+			router.push("/login");
+		} else {
+			const decoded = jwtDecode<DecodedToken>(token);
+			setUserId(decoded.nameid);
+			setAuthenticated(true);
+		}
+	}, [router]);
+
+	return { userId, authenticated };
+};
