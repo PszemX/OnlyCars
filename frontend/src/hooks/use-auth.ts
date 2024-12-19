9bcd779764757ad82103ec13c28764334a082919
mateusz29 <strzeleckimateusz10@gmail.com>
2024-12-19 17:01:34 +0100

    Fix login error

--- frontend/src/hooks/use-auth.ts
+++ frontend/src/hooks/use-auth.ts
@@ -1,31 +1,43 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 import Cookies from "js-cookie";
 import { isTokenExpired } from "@/lib/utils";
 import { jwtDecode } from "jwt-decode";
 
 interface DecodedToken {
-	nameid: string;
+  nameid: string;
 }
 
 export const useAuth = () => {
-	const router = useRouter();
-	const [userId, setUserId] = useState<string | null>(null);
-	const [authenticated, setAuthenticated] = useState<boolean>(false);
+  const router = useRouter();
+  const pathname = usePathname();
+  const [userId, setUserId] = useState<string | null>(null);
+  const [authenticated, setAuthenticated] = useState<boolean>(false);
 
-	useEffect(() => {
-		const token = Cookies.get("token");
-		if (!token || isTokenExpired(token)) {
-			Cookies.remove("token");
-			router.push("/login");
-		} else {
-			const decoded = jwtDecode<DecodedToken>(token);
-			setUserId(decoded.nameid);
-			setAuthenticated(true);
-		}
-	}, [router]);
+  useEffect(() => {
+    const token = Cookies.get("token");
+    const isAuthPage = pathname === '/login' || pathname === '/register';
 
-	return { userId, authenticated };
+    if (!token || isTokenExpired(token)) {
+      Cookies.remove("token");
+      setAuthenticated(false);
+      setUserId(null);
+      
+      if (!isAuthPage) {
+        router.push("/login");
+      }
+    } else {
+      const decoded = jwtDecode<DecodedToken>(token);
+      setUserId(decoded.nameid);
+      setAuthenticated(true);
+
+      if (isAuthPage) {
+        router.push("/");
+      }
+    }
+  }, [router, pathname]);
+
+  return { userId, authenticated };
 };
